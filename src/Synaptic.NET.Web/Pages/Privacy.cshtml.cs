066ba7fb357fd1f9051e52966252e526ad25cf0a
Patrick Weindl <patrick@weindl.org>
2025-08-26 19:30:55 +0200

    chore: unversioned files

---
+++ src/Synaptic.NET.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Synaptic.NET.Web.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
