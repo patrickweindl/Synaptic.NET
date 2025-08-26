c4c3764427d839813967682cc90f3f51f3ebc404
Patrick Weindl <patrick@weindl.org>
2025-08-26 20:41:45 +0200

    chore: cleanup

--- src/Synaptic.NET.Web/WebServices.cs
+++ src/Synaptic.NET.Web/WebServices.cs
@@ -2,7 +2,7 @@ namespace Synaptic.NET.Web;
 
 public static class WebServices
 {
-    static void Main(){}
+    static void Main() { }
 
     public static IHostApplicationBuilder ConfigureWebServices(this IHostApplicationBuilder builder)
     {
