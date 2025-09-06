b9d408b1dae3c8267153de4ce57e46b998b819d9
Patrick Weindl <patrick@weindl.org>
2025-09-06 10:40:52 +0200

    chore: migrations

---
+++ src/Synaptic.NET.Domain/Migrations/20250906084022_ReferenceTypeAdditions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Synaptic.NET.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReferenceTypeAdditions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ReferenceType",
+                table: "Memories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferenceType",
+                table: "Memories");
+        }
+    }
+}
