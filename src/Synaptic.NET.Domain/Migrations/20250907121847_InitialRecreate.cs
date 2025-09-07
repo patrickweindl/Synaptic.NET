ab21cd9932ca46468db55a8dfc38b5b813fca08b
Patrick Weindl <patrick@weindl.org>
2025-09-07 14:19:19 +0200

    chore: missing unversioned files

---
+++ src/Synaptic.NET.Domain/Migrations/20250907121847_InitialRecreate.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Synaptic.NET.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialRecreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Identifier = table.Column<string>(type: "TEXT", maxLength: 512, nullable: false),
+                    DisplayName = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Identifier = table.Column<string>(type: "TEXT", maxLength: 512, nullable: false),
+                    DisplayName = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    Role = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApiKey",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 512, nullable: false),
+                    Key = table.Column<string>(type: "TEXT", maxLength: 1024, nullable: false),
+                    ExpiresAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiKey", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ApiKey_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupMembership",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    GroupId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupMembership", x => new { x.UserId, x.GroupId });
+                    table.ForeignKey(
+                        name: "FK_GroupMembership_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GroupMembership_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MemoryStores",
+                columns: table => new
+                {
+                    StoreId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 512, nullable: false),
+                    Tags = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    GroupId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MemoryStores", x => x.StoreId);
+                    table.ForeignKey(
+                        name: "FK_MemoryStores_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MemoryStores_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Memories",
+                columns: table => new
+                {
+                    Identifier = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 512, nullable: false),
+                    Content = table.Column<string>(type: "TEXT", maxLength: 4096, nullable: false),
+                    Pinned = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Tags = table.Column<string>(type: "TEXT", nullable: false),
+                    ReferenceType = table.Column<int>(type: "INTEGER", nullable: false),
+                    Reference = table.Column<string>(type: "TEXT", maxLength: 2048, nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    Owner = table.Column<Guid>(type: "TEXT", nullable: false),
+                    GroupId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    StoreId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memories", x => x.Identifier);
+                    table.ForeignKey(
+                        name: "FK_Memories_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Memories_MemoryStores_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "MemoryStores",
+                        principalColumn: "StoreId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Memories_Users_Owner",
+                        column: x => x.Owner,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApiKey_UserId",
+                table: "ApiKey",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupMembership_GroupId",
+                table: "GroupMembership",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memories_GroupId",
+                table: "Memories",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memories_Owner",
+                table: "Memories",
+                column: "Owner");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memories_StoreId",
+                table: "Memories",
+                column: "StoreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MemoryStores_GroupId",
+                table: "MemoryStores",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MemoryStores_UserId",
+                table: "MemoryStores",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApiKey");
+
+            migrationBuilder.DropTable(
+                name: "GroupMembership");
+
+            migrationBuilder.DropTable(
+                name: "Memories");
+
+            migrationBuilder.DropTable(
+                name: "MemoryStores");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
