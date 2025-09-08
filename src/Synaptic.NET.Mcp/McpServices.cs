734229eb173f06b862b53cf3ab7b624087453daf
Patrick Weindl <patrick@weindl.org>
2025-09-08 09:17:19 +0200

    feat: addition of non static tool type class and ability to inject additional tools into service builder

--- src/Synaptic.NET.Mcp/McpServices.cs
+++ src/Synaptic.NET.Mcp/McpServices.cs
@@ -2,14 +2,39 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ModelContextProtocol.Protocol;
+using ModelContextProtocol.Server;
 using Synaptic.NET.Domain.Constants;
+using Synaptic.NET.Mcp.Tools;
 
 namespace Synaptic.NET.Mcp;
 
 public static class McpServices
 {
-    public static IHostApplicationBuilder ConfigureMcpServices(this IHostApplicationBuilder builder)
+    public static IHostApplicationBuilder ConfigureMcpServices(
+        this IHostApplicationBuilder builder,
+        Func<IEnumerable<McpServerTool>>? customTools = null,
+        Func<IEnumerable<McpServerResource>>? customResources = null,
+        Func<IEnumerable<McpServerPrompt>>? customPrompts = null)
     {
+        List<McpServerTool> additionalTools = [new NonStaticToolExample()];
+
+        if (customTools != null)
+        {
+            additionalTools.AddRange(customTools.Invoke());
+        }
+
+        List<McpServerResource> additionalResources = new();
+        if (customResources != null)
+        {
+            additionalResources.AddRange(customResources.Invoke());
+        }
+
+        List<McpServerPrompt> additionalPrompts = new();
+        if (customPrompts != null)
+        {
+            additionalPrompts.AddRange(customPrompts.Invoke());
+        }
+
         builder.Services.AddMcpServer(o =>
             {
                 o.ServerInfo = new Implementation
@@ -23,14 +48,18 @@ public static class McpServices
             })
             .WithHttpTransport(o => o.IdleTimeout = TimeSpan.FromMinutes(45))
             .WithToolsFromAssembly()
+            .WithTools(additionalTools)
             .WithResourcesFromAssembly()
-            .WithPromptsFromAssembly();
+            .WithResources(additionalResources)
+            .WithPromptsFromAssembly()
+            .WithPrompts(additionalPrompts);
+
         return builder;
     }
 
     public static WebApplication ConfigureMcpApplicationWithAuthorization(this WebApplication app)
     {
-        app.MapMcp("/mcp").RequireAuthorization();
+        app.MapMcp("/mcp");
         return app;
     }
 }
